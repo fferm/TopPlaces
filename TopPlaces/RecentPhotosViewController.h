4e6ddf9076b53626d40a3597fccc687dd15651e7
Fredrik Ferm <fredrik.ferm@gmail.com>
2013-01-29 00:11:43 +0100

    TabbedViewController as main window

---
+++ TopPlaces/RecentPhotosViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecentPhotosViewController.h
+//  TopPlaces
+//
+//  Created by Fredrik Ferm on 2013-01-29.
+//  Copyright (c) 2013 Fermitet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecentPhotosViewController : UITableViewController
+
+@end
