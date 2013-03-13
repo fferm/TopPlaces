1af67ebdf49635e46909cecca071ed6789e309f2
Fredrik Ferm <fredrik.ferm@gmail.com>
2013-03-14 00:54:43 +0100

    Save to file, read from file, flush directory

--- TopPlaces/PhotoImageStorageManager.m
+++ TopPlaces/PhotoImageStorageManager.m
@@ -15,6 +15,7 @@
 #define IMAGE_CACHE_DIR @"imageCache"
 #define IMAGE_EXTENSION @"full"
 #define CALLOUT_EXTENSION @"callout"
+#define MAX_SIZE 10 * 1000 * 1000     // 10 MB
 
 -(NSFileManager *)fileManager{
     return [[NSFileManager alloc] init];
@@ -30,33 +31,93 @@
 
 -(UIImage *)uiImageForPhoto:(Photo *)photo imageYesCalloutNo:(BOOL)imageYesCalloutNo{
     NSString *path = [self pathForPhoto:photo imageYesCalloutNo:imageYesCalloutNo];
-    
-    UIImage *ret;
+    NSData *data;
     
     // Check if file already exists
     if ([self.fileManager fileExistsAtPath:path]) {
         // if it does, read and return it
-        NSLog(@"Exists");
+        data = [NSData dataWithContentsOfFile:path];
+        
     } else {
         // if it doesn't: download, possibly flush and save
-        NSData *data = [self downloadImage:photo imageYesCalloutNo:imageYesCalloutNo];
-        ret = [UIImage imageWithData:data];
-        
-        NSLog([self pathForPhoto:photo imageYesCalloutNo:imageYesCalloutNo]);
-        [data writeToFile:[self pathForPhoto:photo imageYesCalloutNo:imageYesCalloutNo] atomically:YES];
-        // TODO: flush 
+        data = [self downloadImage:photo imageYesCalloutNo:imageYesCalloutNo];
+
+        [self writeData:data atPath:path];
+
+        [self flushDirectory];
+    }
+    return [UIImage imageWithData:data];
+}
+
+-(void)writeData:(NSData *)data atPath:(NSString *)path{
+    BOOL isDir;
+    if (! ([self.fileManager fileExistsAtPath:[self dirPath] isDirectory:&isDir] && isDir)) {
+        BOOL success = [self.fileManager createDirectoryAtPath:[self dirPath] withIntermediateDirectories:YES attributes:nil error:nil];
+        if (!success) {
+            NSLog(@"Error trying to create directory %@", [self dirPath]);
+            return;
+        }
     }
     
-    return ret;
+    BOOL success = [self.fileManager createFileAtPath:path contents:data attributes:nil];
+    if (!success) {
+        NSLog(@"Error trying to create file %@", path);
+        return;
+    }
+}
+
+-(void)flushDirectory{
+    NSArray *fileNames = [self.fileManager contentsOfDirectoryAtPath:[self dirPath] error:nil];
+    
+    // Get attributes of all files, simultaneously add up sizes to check if we need to do more
+    NSMutableDictionary *attributesByFilename = [NSMutableDictionary dictionary];
+    
+    long long totalSize = 0;
+    for (NSString *fileName in fileNames) {
+        NSDictionary *attributes = [self.fileManager attributesOfItemAtPath:[[self dirPath] stringByAppendingPathComponent:fileName] error:nil];
+        [attributesByFilename setObject:attributes forKey:fileName];
+        long long size = [(NSNumber *)[attributes objectForKey:NSFileSize] longLongValue];
+        totalSize += size;
+    }
+    
+    if (totalSize < MAX_SIZE) return;
+    
+    // Sort files by date
+    NSMutableArray *fileNamesSortedByDateAscending = [fileNames mutableCopy];
+    [fileNamesSortedByDateAscending sortUsingComparator:^NSComparisonResult(id obj1, id obj2){
+        NSString *fileName1 = (NSString *)obj1;
+        NSString *fileName2 = (NSString *)obj2;
+        
+        NSDictionary *attributes1 = [attributesByFilename objectForKey:fileName1];
+        NSDictionary *attributes2 = [attributesByFilename objectForKey:fileName2];
+        
+        NSDate *date1 = [attributes1 objectForKey:NSFileCreationDate];
+        NSDate *date2 = [attributes2 objectForKey:NSFileCreationDate];
+        
+        return [date1 compare:date2];
+    }];
+    
+    long long flushNeeded = totalSize - MAX_SIZE;
+    long long flushedSoFar = 0;
+    for (NSString *fileName in fileNamesSortedByDateAscending) {
+        NSDictionary *attributes = [attributesByFilename objectForKey:fileName];
+        long long size = [(NSNumber *)[attributes objectForKey:NSFileSize] longLongValue];
+        
+        [self.fileManager removeItemAtPath:[[self dirPath] stringByAppendingPathComponent:fileName] error:nil];
+        
+        flushedSoFar += size;
+
+        if (flushedSoFar > flushNeeded) break;
+    }
 }
 
 -(NSString *)dirPath{
-    return [[self.fileManager URLsForDirectory:NSCachesDirectory inDomains:NSUserDomainMask].lastObject path];
+    return [[[self.fileManager URLsForDirectory:NSCachesDirectory inDomains:NSUserDomainMask].lastObject path] stringByAppendingPathComponent:IMAGE_CACHE_DIR];
 }
 
 -(NSString *)pathForPhoto:(Photo *)photo imageYesCalloutNo:(BOOL)imageYesCalloutNo {
     NSString *extension = imageYesCalloutNo ? IMAGE_EXTENSION : CALLOUT_EXTENSION;
-    return [[[[self dirPath] stringByAppendingPathComponent:IMAGE_CACHE_DIR] stringByAppendingPathComponent:photo.photoId] stringByAppendingPathExtension:extension];
+    return [[[self dirPath] stringByAppendingPathComponent:photo.photoId] stringByAppendingPathExtension:extension];
 }
 
 
